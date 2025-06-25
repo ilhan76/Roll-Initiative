59884e7e8c47c85bba36c24e07e3d2936072751d
WiM <noemail@mail.this>
2025-06-25 10:33:20 +0300

    Добавил compose для блок для ячейки для характеристик

---
+++ app/src/main/java/ru/kudashov/rollinitiative/ui/element/characteristics/CharacteristicsBlock.kt
@@ -0,0 +1,79 @@
+package ru.kudashov.rollinitiative.ui.element.characteristics
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.ui.draw.clip
+import ru.kudashov.rollinitiative.ui.theme.UiKitTheme
+
+@Composable
+fun StatCell(
+    modifier: Modifier = Modifier,
+    title: String,
+    value: Int,
+    bonus: Int?
+) {
+    val topColor = UiKitTheme.colors.component2()
+    val bottomColor = UiKitTheme.colors.component1()
+    val cornerShape = RoundedCornerShape(15.dp)
+
+    Column(
+        modifier = modifier
+            .width(95.dp)
+            .clip(cornerShape)
+            .border(1.dp, Color.Gray.copy(alpha = 0.2f), cornerShape)
+            .background(bottomColor)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(topColor)
+                .padding(vertical = 4.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = title,
+                color = UiKitTheme.colors.primary,
+                style = UiKitTheme.typography.subtitle1
+            )
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 4.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            val display = if (bonus != null) "$value (+$bonus)" else "$value"
+            Text(
+                text = display,
+                color = UiKitTheme.colors.secondary,
+                style = UiKitTheme.typography.subtitle2
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun CharacterStatCellPreview() {
+    UiKitTheme(darkTheme = true) {
+        StatCell(
+            title = "Strength",
+            value = 1,
+            bonus = 1,
+            modifier = Modifier
+        )
+    }
+}
