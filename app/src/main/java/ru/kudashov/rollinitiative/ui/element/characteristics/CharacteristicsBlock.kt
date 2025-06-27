c382009715247129768b54b49c1d9045b753e641
WiM <noemail@mail.this>
2025-06-27 14:48:16 +0300

    Заданный размер заменил на fillMaxWidth, убрал неиспользуемые импорты

--- app/src/main/java/ru/kudashov/rollinitiative/ui/element/characteristics/CharacteristicsBlock.kt
+++ app/src/main/java/ru/kudashov/rollinitiative/ui/element/characteristics/CharacteristicsBlock.kt
@@ -1,19 +1,16 @@
 package ru.kudashov.rollinitiative.ui.element.characteristics
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.ui.draw.clip
 import ru.kudashov.rollinitiative.ui.theme.UiKitTheme
@@ -31,7 +28,7 @@ fun StatCell(
 
     Column(
         modifier = modifier
-            .width(95.dp)
+            .fillMaxWidth()
             .clip(cornerShape)
             .background(bottomColor)
     ) {
@@ -45,7 +42,7 @@ fun StatCell(
             Text(
                 text = title,
                 color = UiKitTheme.colors.primary,
-                style = UiKitTheme.typography.subtitle1
+                style = UiKitTheme.typography.caption2
             )
         }
         Box(
@@ -69,7 +66,7 @@ fun StatCell(
 fun CharacterStatCellPreview() {
     UiKitTheme(darkTheme = true) {
         StatCell(
-            title = "Strength",
+            title = "СИЛ",
             value = 1,
             bonus = 1,
             modifier = Modifier
