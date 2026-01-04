4c8f1c3cb7874d77edfc9592f8152778c4f5bda1
kudashov <kudashov@surf.dev>
2026-01-04 17:57:34 +0300

    Правки

--- core-ui/compose/uikit/src/main/kotlin/ru/rollinitiative/core/ui/compose/uikit/icons/ArrowBack.kt
+++ core-ui/compose/uikit/src/main/kotlin/ru/rollinitiative/core/ui/compose/uikit/icons/ArrowBack.kt
@@ -7,7 +7,7 @@ import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.graphics.vector.path
 import androidx.compose.ui.unit.dp
-import ru.rollinitiative.ui.theme.UiKitIcon
+import ru.rollinitiative.core.ui.compose.uikit.theme.UiKitIcon
 
 val UiKitIcon.ArrowBack: ImageVector by lazy {
     ImageVector.Builder(
